7ef656f0aa1eca0e4948747f185c541251d1ef1d
Juliebambou <bocquenet.julie@gmail.com>
2024-02-15 17:56:45 +0100

    ok

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,9 +1,8 @@
 import styles from '../styles/Home.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addBookmark, removeBookmark } from '../reducers/bookmarks';
-import {user}} from '../reducers/hiddenArticles';
+/*import { addBookmark, removeBookmark } from '../reducers/bookmarks';
+import {user} from '../reducers/hiddenArticles';*/
 import Image from 'next/image';
-import styles from '../styles/Article.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
@@ -12,25 +11,24 @@ function Tweet() {
   const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
 
-  const handleLikeTweet = 
+  const handleLikeTweet = '';
 
 
   return (
-
     <div>
-      <main className={styles.main}> 
+      <main className={styles.main}>
         <div>
-			    <img className={styles.avatar} src={props.image} />		
+			    <img className={styles.avatar} src={props.image} />
           <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeTweet()} style={heartIconStyle} className="like" /></span>
         </div>
 			<h2 className={styles.name}>{props.name}</h2>
 			<div className={styles.btnContainer}>
 			<button className={styles.selectBtn} onClick={() => handleClick()}>Select</button>
 			<button className={styles.removeBtn} onClick={() => removeClick()}>Remove all</button>
-		
+
 		</div>
 
-        
+
       </main>
     </div>
   );
---
+++ pages/messages.js
@@ -0,0 +1,28 @@
+import Trends from '../components/Trends';
+import Tweet from '../components/Tweet';
+import LastTweets from '../components/LastTweets';
+import Menu from '../components/Menu';
+import styles from '../styles/Messages.module.css';
+
+function messages() {
+  return (
+    <div className={styles.menu} >
+
+        <div className={styles.leftcontent}>
+            <Menu/>
+        </div>
+
+        <div className={styles.middlecontent}>
+           <Tweet/>
+            <LastTweets/>
+        </div>
+
+        <div className={styles.rightcontent}>
+            <Trends/>
+        </div>
+
+  </div>
+  )
+};
+
+export default messages;
