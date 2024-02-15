3560405cbac26b1a909050fa8fcd61440081f6c0
Manon <audibertmanon@hotmail.com>
2024-02-15 17:52:26 +0100

    reducers

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,36 +1,50 @@
 import styles from '../styles/Home.module.css';
+import {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addBookmark, removeBookmark } from '../reducers/bookmarks';
-import {user}} from '../reducers/hiddenArticles';
-import Image from 'next/image';
-import styles from '../styles/Article.module.css';
+import {user} from '../reducers/user';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faTrashCan, } from '@fortawesome/free-solid-svg-icons';
+import logo from '../public/avatar.png';
 
 function Tweet() {
 
   const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
+  const [likeCount, setLlikeCount] = useState(0);
 
-  const handleLikeTweet = 
 
+  const handleLikeTweet = () => {
+    setLlikeCount(likeCount + 1);
+  };
+   let heartIconStyle = { 'cursor': 'pointer' };
+   if(likeCount > 0)
+     {
+    heartIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
+  };
+
+
+  // const deleteTweet = () => {
+  //   props.removeTweet(props.message)
+	// };
+
+
+  
 
   return (
 
-    <div>
+    <div className={styles.container}>
       <main className={styles.main}> 
-        <div>
-			    <img className={styles.avatar} src={props.image} />		
-          <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeTweet()} style={heartIconStyle} className="like" /></span>
-        </div>
-			<h2 className={styles.name}>{props.name}</h2>
-			<div className={styles.btnContainer}>
-			<button className={styles.selectBtn} onClick={() => handleClick()}>Select</button>
-			<button className={styles.removeBtn} onClick={() => removeClick()}>Remove all</button>
-		
-		</div>
-
-        
+          <div>
+			      <img className={styles.avatar} src={logo} alt="avatar" />
+           <p>pseudo @</p>		
+          </div>
+            <div>
+             <p>Ici je tweet</p>
+           </div>
+            {/* <div> */}
+             {/* <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeTweet()} style={heartIconStyle} className="like" /></span>
+             <span><FontAwesomeIcon icon={faTrashCan} onClick={() => deleteTweet()} style={trashIconStyle} className="trash" /></span>
+            </div>        */}
       </main>
     </div>
   );
